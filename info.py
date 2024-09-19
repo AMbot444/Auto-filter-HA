a114fe9770c9bfa894164d7cd42e4e7606360829
AMbot444 <amitbhati51047@gmail.com>
2024-09-19 10:04:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,7 +64,7 @@ else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://amitbhati9423:9ct8hr45jNDAHtfO@cluster0.mzeql.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
