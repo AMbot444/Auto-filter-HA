000e59173a2e9fa21dae46575b21d794cadeff6d
AMbot444 <amitbhati51047@gmail.com>
2024-10-14 23:04:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,7 +80,7 @@ TUTORIAL = environ.get("TUTORIAL", "https://t.me/HA_Bots")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/HA_Bots")
 
 # Bot settings
-DELETE_TIME = int(environ.get('DELETE_TIME', 1800)) # Add time in seconds
+DELETE_TIME = int(environ.get('DELETE_TIME', 300)) # Add time in seconds
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 MAX_BTN = int(environ.get('MAX_BTN', 10))
 LANGUAGES = [language.lower() for language in environ.get('LANGUAGES', 'hindi english telugu tamil kannada malayalam marathi punjabi').split()]
